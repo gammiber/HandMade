6b216f0bc2617bf1c5cd543b06900147d640f851
Ekaterina <mdmrogova@gmail.com>
2015-06-02 16:42:14 +0300

    Create project

---
+++ HM/HM/Areas/ArtisanList/ArtisanListAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HM.Areas.ArtisanList
+{
+    public class ArtisanListAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ArtisanList";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ArtisanList_default",
+                "ArtisanList/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
