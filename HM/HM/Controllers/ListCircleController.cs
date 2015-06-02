ead0bbcc1ca54a2106e4995d0ca6081fb51e0ca4
Ekaterina <mdmrogova@gmail.com>
2015-06-02 19:21:36 +0300

    Add list top, add list category

---
+++ HM/HM/Controllers/ListCircleController.cs
@@ -0,0 +1,37 @@
+﻿using HM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HM.Controllers
+{
+    public class ListCircleController : Controller
+    {
+        private dbHandMadeEntities db = new dbHandMadeEntities();
+        // GET: ListCircle
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //View top artisans in base
+        public ActionResult TopList(string city)
+        {
+            var artisans = 
+                (from artisan in db.Artisans 
+                 where artisan.City == city
+                 select artisan)
+                .ToList();
+            return View(artisans);
+        }
+
+        //View categories
+        public ActionResult CategoryList()
+        {
+            var categories = (from category in db.Categories select category).ToList();
+            return View(categories);
+        }
+    }
+}
